6931f493565f1e741a2cecb0d6c14a169c5f91de
TheJade <TheJade@users.noreply.github.com>
2020-02-04 15:25:41 -0700

    Start of the Conductor Game
    Not Finished still needs some design work and stuff

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -61,7 +61,7 @@ Variable names shall start with "UserApp1_<type>" and be declared as static.
 ***********************************************************************************************************************/
 static fnCode_type UserApp1_pfStateMachine;               /*!< @brief The state machine function pointer */
 //static u32 UserApp1_u32Timeout;                           /*!< @brief Timeout counter used across states */
-
+static void Game1 (void);
 
 /**********************************************************************************************************************
 Function Definitions
@@ -107,7 +107,7 @@ void UserApp1Initialize(void)
   
   if( 1 )
   {
-    UserApp1_pfStateMachine = UserApp1SM_Idle;
+    UserApp1_pfStateMachine = Game1;
   }
   else
   {
@@ -116,22 +116,6 @@ void UserApp1Initialize(void)
   }
 } /* end UserApp1Initialize() */
 
-  
-/*!----------------------------------------------------------------------------------------------------------------------
-@fn void UserApp1RunActiveState(void)
-
-@brief Selects and runs one iteration of the current state in the state machine.
-
-All state machines have a TOTAL of 1ms to execute, so on average n state machines
-may take 1ms / n to execute.
-
-Requires:
-- State machine function pointer points at current state
-
-Promises:
-- Calls the function to pointed by the state machine function pointer
-
-*/
 void UserApp1RunActiveState(void)
 {
   UserApp1_pfStateMachine();
@@ -139,33 +123,22 @@ void UserApp1RunActiveState(void)
 } /* end UserApp1RunActiveState */
 
 
-/*------------------------------------------------------------------------------------------------------------------*/
-/*! @privatesection */                                                                                            
-/*--------------------------------------------------------------------------------------------------------------------*/
 
+/*game in C
+first edition will likely need to change to debug for board
+This is going to be a sound timer game
+User will be required to try to time the beat
+Hit the button at the same time as the beat 
+Led indicators on for the beat time
+*/
 
-/**********************************************************************************************************************
-State Machine Function Definitions
-**********************************************************************************************************************/
-/*-------------------------------------------------------------------------------------------------------------------*/
-/* What does this state do? */
-static void UserApp1SM_Idle(void)
+/*version 1 of the code is buggy and can be found in the Games section of 
+the GitHub folder on my laptop, i just copy pasta it into her for record purposes
+*/
+
+static void Game1(void)
 {
-  static u16 u16WaveHeight = 0;
   
-  if (G_u32SystemTime1ms % 80 == 0)//change num this to change rate of wave
-  {
-    u16WaveHeight++;
-    LedPWM(WHITE, LED_PWM_75 - (u16WaveHeight % 8) * 2);
-    LedPWM(PURPLE, LED_PWM_75 - ((u16WaveHeight + 1) % 8) * 2);
-    LedPWM(BLUE, LED_PWM_75 - ((u16WaveHeight + 2) % 8) * 2);
-    LedPWM(CYAN, LED_PWM_75 - ((u16WaveHeight + 3)% 8) * 2);
-    LedPWM(GREEN, LED_PWM_75 - ((u16WaveHeight + 4) % 8) * 2);
-    LedPWM(YELLOW, LED_PWM_75 - ((u16WaveHeight + 5)  % 8) * 2);
-    LedPWM(ORANGE, LED_PWM_75 - ((u16WaveHeight + 6) % 8) * 2);
-    LedPWM(RED, LED_PWM_75 - ((u16WaveHeight + 7) % 8) * 2);
-    //the wave travels from right to left and it bright to dark
-  }
 } /* end UserApp1SM_Idle() */
      
 
@@ -173,7 +146,90 @@ static void UserApp1SM_Idle(void)
 /* Handle an error */
 static void UserApp1SM_Error(void)          
 {
-  
+  //should increase in difficulty
+    //speed increases slowly
+    //first gives about 5 button presses until
+    //start
+    //sound turns off eventually
+    //leds turn off eventually
+    static int FirstTime = 1;
+    if (FirstTime = 1) //so that id doesn't display each loop
+    {
+        LcdMessage(LINE1_START_ADDR, "Welcome 2 Conductor");
+        LcdMessage(LINE2_START_ADDR, "Start     "); //need to test out the spaces after
+    }
+    LcdMessage(LINE2_END_ADDR - 3, "QUIT"); //Will continue to be here
+
+    static int GameLevel = 0;   //difficulty will increase
+    static int startTime = 0;   //start timeing for the beats
+    static int Display = 0; //will be 1 if a average time should be shown to the user
+                            //will be count down from 11 to 6 (5) button presses to start
+                            //will then count from 6 to 1 when user is being timed
+    int forgivness = 300 - (GameLevel * 25); //In ms The closeness the user has to be to the beat divided by two to pass it
+                    //this is an import to adjust the difficulty
+    static int interval = 500; //in ms //the time between beats
+                    //adjust this line sometimes don't know when yet
+    static int averageTime = 0; //should change on the real button presses the user has
+                    //build a equation later on for this when Display = 1
+                    //should be displayed to the user
+
+    if (GameLevel > 0)  //functions will activate/disable depending on level
+    {
+        //NEED TO:
+            //reset the startTime and and average time
+
+        if (startTime == 0)//sets all the values to start
+        {
+            Display = 11; // begins the count down
+            startTime = G_u32SystemTime1ms; //sets the start time
+        }
+
+        if ((Display > 1) && ((G_u32SystemTime1ms - startTime) % interval <= 6))//preform the function for 6 ms
+        {                               //may need to adjust the parameteres of the if
+            //buzzer sounds for 6 ms    //or something //maybe change for each level
+
+            //LED maybe the same time
+        }
+
+        if (Display > 1 && Display < 7 && (WasButtonPressed(BUTTON0) || WasButtonPressed(BUTTON1) || WasButtonPressed(BUTTON2)))    //average acumulation
+        {
+            ButtonAcknowledge(BUTTON0);
+            ButtonAcknowledge(BUTTON1);
+            ButtonAcknowledge(BUTTON2);
+
+            averageTime = ((G_u32SystemTime1ms - startTime) % interval - 3); //the time difference I think //need to adjust to make the average
+
+
+        }
+
+
+        if (GameLevel < 5) //LED     //will need to make adjustments on the activation times    //may change this
+        {
+            //LEDs flash on at the press time should last the duration of the forgivness of the level
+        }
+
+        
+
+    } //end game run "function"
+
+    if ((Display > 1) && (Display < 7)) //disoplays the current average time
+    {
+        //turns off after 3 seconds
+    }
+
+    else if (WasButtonPressed(BUTTON0)) //start game option
+    {
+        LcdMessage(LINE1_START_ADDR, "                   "); //coverse the previous message
+        ButtonAcknowledge(BUTTON0);
+        GameLevel = 1;
+        FirstTime = 0; //incase the user pressed the button during welcome message
+    }
+    if (WasButtonPressed(BUTTON3)) //Quit option should show final score (level made)
+    {
+        UserApp1_pfStateMachine = ChooseGame; //when integrated with "RandomNumberGenertor" will work well
+    }
+    
+    LCDMessage(LINE1_START_ADDR, " ");
 } /* end UserApp1SM_Error() */
 
 
