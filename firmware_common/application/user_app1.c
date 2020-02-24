640163c58d7b7c0a29c6ff8bb999a88817a4f451
TheJade <TheJade@users.noreply.github.com>
2020-02-24 13:56:36 -0700

    Just some UI planning
    just some ideas for the UI. Just some ideas on the formatting. No implimentation yet

--- firmware_common/application/user_app1.c
+++ firmware_common/application/user_app1.c
@@ -1,6 +1,69 @@
 //simple master for the games
 
-//version: LABEL_THE_REPOSITORY
+//version: UIForTexasHoldem
+
+//user will be givin 2 "random" cards then be asked to: check, bet, fold
+//  check will cause no bet and the player turn is over
+//  bet will have a submenu/suboptions, containing buttons of less, more, all in
+//  fold will continue the round but player is out
+/*example of UI
+  H = hearts
+  C = clubs
+  D = dimonds
+  S = spades
+
+  Q = quit to save the game (feature to add)
+Deal:
+    ______________________
+    |7H 8S|      7S AC 2H|                 
+    |Check  Bet   Fold  Q|                    
+    ______________________
+    O      O       O     O
+    ______________________
+    |7H 8S|   9H 7S AC 2H|                 
+    |Check  Bet   Fold  Q|                    
+    ______________________
+    O      O       O     O
+    ______________________
+    |7H 8S|JD 9H 7S AC 2H|                 
+    |Check  Bet   Fold  Q|                    
+    ______________________
+    O      O       O     O
+BetSubmenu:
+    I think the top one looks better with the whole back word
+    ______________________
+    |Pot: 5320   Bet: 350|             
+    |Up  Down  Enter Back|                    
+    ______________________
+    O      O       O     O
+    ______________________
+    |Pot: 5320   Bet: 350|             
+    |Up  Down  Enter    B|                    
+    ______________________
+    O      O       O     O
+
+End of round (show hands):
+
+    ______________________
+    |   JD 9H 7S AC 2H   |  //river at top           
+    |7H 8S| 6H 5H | 4S KS|  //hands at bottom, player hand on far right              
+    ______________________
+    O      O       O     O
+    ______________________
+    |   JD 9H 7S AC 2H   |  //river at top           
+    |      |7H 8S|       |  //winner hand more to middle            
+    ______________________
+    O      O       O     O
+    ______________________
+    |   Player 1 Wins    |             
+    |     Pot: 5320      |                    
+    ______________________
+    O      O       O     O
+
+
+
+
+*/
 
 #include "configuration.h"
 
@@ -22,17 +85,21 @@ static fnCode_type UserApp1_pfStateMachine;               /*!< @brief The state 
 //states initialized
 static void Startup(void);
 static void MainState(void);
+
+struct xorwow_state;
 //--------------------------------------put--the--version--code--here----------------------------------------------------
 //new initialized states:
 
-void TestFunction2(void);// To be removed just an example of using a fuction from another filej
-void TestFunction3(void);// To be removed just an example of using a fuction from another filej
+void TestFunction2(void);// To be removed just an example of using a fuction from another file
+void TestFunction3(void);// To be removed just an example of using a fuction from another file
 
 //-----------------------------------------------------------------------------------------------------------------------
 
-//Put global veribals here
-u32 G_u32Seed = 0; //Stores the First button press to help genertate a "random number"
-u32 TimePressSecond = 0;
+//Structures here
+struct xorwow_state{  //stores the values for the Random Number Generator
+  u32 a, b, c, d;
+  u32 counter;
+}seed;
 
 void UserApp1Initialize(void)
 {
@@ -48,6 +115,12 @@ void UserApp1Initialize(void)
   LedOff(RED);
   LcdMessage(LINE1_START_ADDR, "Basic Menu Start    ");
   LcdMessage(LINE2_START_ADDR, "Press any button    ");
+  seed.a = 580938204;
+  seed.b = 325414048;
+  seed.c = 486034763;
+  seed.d = 346141525;
+
+
   
   TestFunction2();// To be removed just an example of using a fuction from another file
   TestFunction3();// To be removed just an example of using a fuction from another file
@@ -76,21 +149,21 @@ static void Startup(void) //basic menu system to generate the seed (at this poin
 
   if (WasButtonPressed(BUTTON0))
   {
-    G_u32Seed = G_u32SystemTime1ms;
+    seed.a = G_u32SystemTime1ms;
     UserApp1_pfStateMachine = MainState;
     ButtonAcknowledge(BUTTON0);
     
   }
   else if (WasButtonPressed(BUTTON1))
   {
-    G_u32Seed = G_u32SystemTime1ms + 250;
+    seed.b = G_u32SystemTime1ms + 250;
     UserApp1_pfStateMachine = MainState;
     ButtonAcknowledge(BUTTON1);
     
   }
   else if (WasButtonPressed(BUTTON2))
   {
-    G_u32Seed = G_u32SystemTime1ms + 500;
+    seed.c = G_u32SystemTime1ms + 500;
     UserApp1_pfStateMachine = MainState;
     ButtonAcknowledge(BUTTON2);
     
@@ -98,7 +171,7 @@ static void Startup(void) //basic menu system to generate the seed (at this poin
      
   else if (WasButtonPressed(BUTTON3))
   {
-    G_u32Seed = G_u32SystemTime1ms + 750;
+    seed.d = G_u32SystemTime1ms + 750;
     UserApp1_pfStateMachine = MainState;
     ButtonAcknowledge(BUTTON3);
     
@@ -108,6 +181,7 @@ static void Startup(void) //basic menu system to generate the seed (at this poin
 static void MainState(void)
 {
   LedOn(GREEN); //to indicate the MainState is being run
+  
 }
 //-----------------------------------------------------------------------------------------------------------------------
 
